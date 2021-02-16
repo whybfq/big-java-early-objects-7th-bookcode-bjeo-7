8d79c60a4dd2d57289c0be0084807f3cd8b3a723
whybfq <littlesanner@gmail.com>
2021-02-16 15:03:39 +0700

    javadoc Counter01.java succeed to generate the Counter01.html Try to solve java: duplicate class: BankAccount

--- ch05/sec03/EarthquakeRunner.java
+++ ch05/sec03/EarthquakeRunner.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
    This program prints a description of an earthquake of a given magnitude.
 */
 public class EarthquakeRunner
-{  
+{
+//   public enum F {SINGLE, MARRIED, MARRIED_FILING};
+
    public static void main(String[] args)
    {  
       Scanner in = new Scanner(System.in);
