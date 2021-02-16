304857bd84f10e7734e56a1f325f18742b482f3b
whybfq <littlesanner@gmail.com>
2021-02-16 13:52:13 +0700

    javadoc Counter01.java succeed to generate the Counter01.html Try to solve java: duplicate class: BankAccount

--- ch03/sec08/CarComponent.java
+++ ch03/sec08/CarComponent.java
@@ -8,7 +8,7 @@ import javax.swing.JComponent;
 public class CarComponent extends JComponent
 {  
    public void paintComponent(Graphics g)
-   {  
+   {
       Graphics2D g2 = (Graphics2D) g;
 
       Car car1 = new Car(0, 0);
--- ch04/how_to_1/VendingMachine.java
+++ ch04/how_to_1/VendingMachine.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 /**
